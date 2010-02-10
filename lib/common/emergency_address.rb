4ec5836ecb16294588beb5f7db5dab6aaeb1c973
Andrew Roth <andrewroth@gmail.com>
2010-02-10 00:58:27 -0500

    make emergency_address common modeled

---
+++ lib/common/emergency_address.rb
@@ -0,0 +1,13 @@
+module Common
+  module EmergencyAddress
+    def self.included(base)
+      base.class_eval do
+        before_create :set_address_type
+      end
+    end
+
+    def set_address_type
+      self.address_type = "emergency1"
+    end
+  end
+end
