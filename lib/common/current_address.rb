2435423d07ab081d1d826667504fe4104caf3eb6
Andrew Roth <andrewroth@gmail.com>
2010-02-10 00:47:58 -0500

    make current_address common modeled

---
+++ lib/common/current_address.rb
@@ -0,0 +1,17 @@
+module Common
+  module CurrentAddress
+    def self.included(base)
+      base.class_eval do
+        load_mappings
+        validates_format_of   _(:email),
+                              :with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                              :message    => 'must be valid'
+        before_create :set_address_type
+      end
+    end
+
+    def set_address_type
+      self.address_type = "current"
+    end
+  end
+end
