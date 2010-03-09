dd8bb196952747fe5f0a7deac6d83735d09797dd
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-09 17:33:53 -0500

    Move code out of CDM that doesn't belong

--- lib/common/core/campus_involvement.rb
+++ lib/common/core/campus_involvement.rb
@@ -25,10 +25,6 @@ module Common
 
       def archived?() end_date.present? end
 
-      def new_student_history
-        StudentInvolvementHistory.new :person_id => person_id, :campus_id => campus_id, :school_year_id => school_year_id, :end_date => Date.today, :ministry_role_id => find_or_create_ministry_involvement.ministry_role_id, :start_date => (last_history_update_date || start_date), :campus_involvement_id => id
-      end
-
       def derive_ministry
         # look for the latest MC, under the assumption it will be the most nested
         # if staff start wanting to have staff-only groups with campuses, we'll have to
