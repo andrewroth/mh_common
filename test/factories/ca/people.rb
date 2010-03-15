cc336429b493ea84e8b2f2b5c8a24e5f3aa3fa09
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-15 11:11:38 -0400

    Modify people factory to work with CA schema

--- test/factories/ca/people.rb
+++ test/factories/ca/people.rb
@@ -1,32 +1,32 @@
 Factory.define :person_1, :class => Person, :singleton => true do |p|
   p.id '50000'
-  p.user_id '1'
   p.first_name 'Josh'
+  p.person_legal_fname 'Josh'
   p.last_name 'Starcher'
+  p.person_legal_lname 'Starcher'
   p.middle_name 'Lee'
   p.preferred_name 'Josh'
-  p.level_of_school 'Undergrad'
-  p.graduation_date '06/15/2004'
   p.major 'Philosophy'
   p.minor 'Computer Science'
   p.birth_date '07/07/1982'
-  p.bio "I\'m an MK"
   p.gender '1'
 end
 
 Factory.define :person_2, :class => Person, :singleton => true do |p|
   p.id '3000'
   p.first_name 'fred'
+  p.person_legal_fname 'fred'
   p.last_name 'anderson'
-  p.user_id '2'
+  p.person_legal_lname 'anderson'
   p.gender 'M'
 end
 
 Factory.define :person_3, :class => Person, :singleton => true do |p|
   p.id '2000'
   p.first_name 'sue'
+  p.person_legal_fname 'sue'
   p.last_name 'johnson'
-  p.user_id '3'
+  p.person_legal_lname 'johnson'
   p.gender 'F'
 end
 
@@ -41,34 +41,41 @@ end
 Factory.define :person, :class => Person do |c|
   c.id { Factory.next(:person_person_id) }
   c.first_name 'A'
+  c.person_legal_fname 'A'
   c.last_name { Factory.next(:person_last_name) }
+  c.person_legal_lname { |c| c.last_name }
 end
 
 Factory.define :person_5, :class => Person, :singleton => true do |p|
   p.id '4000'
-  p.user_id '4'
   p.first_name 'NoMinistry'
+  p.person_legal_fname 'NoMinistry'
   p.last_name 'Involvements'
+  p.person_legal_lname 'Involvements'
 end
 
 Factory.define :person_6, :class => Person, :singleton => true do |p|
   p.id '4001'
-  p.user_id '5'
   p.first_name '\'Ministry\''
+  p.person_legal_fname '\'Ministry\''
   p.last_name '\'Leader\''
+  p.person_legal_lname '\'Leader\''
 end
 
 Factory.define :person_7, :class => Person, :singleton => true do |p|
   p.id '4002'
-  p.user_id '6'
   p.first_name '\'Ministry\''
+  p.person_legal_fname '\'Ministry\''
   p.last_name '\'Leader\''
+  p.person_legal_lname '\'Leader\''
 end
 
 Factory.define :person_111, :class => Person, :singleton => true do |p|
   p.id '111'
   p.first_name 'Random'
+  p.person_legal_fname 'Random'
   p.last_name 'Person'
+  p.person_legal_lname 'Person'
 end
 
 
