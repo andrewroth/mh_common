64cce51ce1f9d5e6b4bf2da66db62fc82ca3f57c
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-24 12:25:37 -0400

    Add accessgroup and school_year factories

--- test/factories/ca/school_years.rb
+++ test/factories/ca/school_years.rb
@@ -1,9 +1,49 @@
 Factory.define :schoolyear_1, :class => SchoolYear, :singleton => true do |s|
   s.id '1'
-  s.name 'Freshman'
+  s.name '1st Year (Undergrad)'
 end
 
 Factory.define :schoolyear_2, :class => SchoolYear, :singleton => true do |s|
   s.id '2'
-  s.name 'Sophomore'
+  s.name '2nd Year (Undergrad)'
+end
+
+Factory.define :schoolyear_3, :class => SchoolYear, :singleton => true do |s|
+  s.id '3'
+  s.name '3rd Year (Undergrad)'
+end
+
+Factory.define :schoolyear_4, :class => SchoolYear, :singleton => true do |s|
+  s.id '4'
+  s.name '4th Year (Undergrad)'
+end
+
+Factory.define :schoolyear_5, :class => SchoolYear, :singleton => true do |s|
+  s.id '5'
+  s.name '5th Year (Undergrad)'
+end
+
+Factory.define :schoolyear_6, :class => SchoolYear, :singleton => true do |s|
+  s.id '6'
+  s.name '1st Year (Grad)'
+end
+
+Factory.define :schoolyear_7, :class => SchoolYear, :singleton => true do |s|
+  s.id '7'
+  s.name '2nd Year (Grad)'
+end
+
+Factory.define :schoolyear_8, :class => SchoolYear, :singleton => true do |s|
+  s.id '8'
+  s.name '3rd Year (Grad)'
+end
+
+Factory.define :schoolyear_9, :class => SchoolYear, :singleton => true do |s|
+  s.id '9'
+  s.name 'Other'
+end
+
+Factory.define :schoolyear_10, :class => SchoolYear, :singleton => true do |s|
+  s.id '10'
+  s.name 'Alumni'
 end
