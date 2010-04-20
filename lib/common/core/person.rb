1c3652a35280006818c942f47cb60b0908d2db49
Andrew Roth <andrewroth@gmail.com>
2010-04-20 14:14:04 -0400

    fix is_staff_somewhere?

--- lib/common/core/person.rb
+++ lib/common/core/person.rb
@@ -279,6 +279,12 @@ module Common
         end
       end
       
+      def is_staff_somewhere?
+        ::MinistryInvolvement.find(:first, :conditions =>
+           ["#{_(:person_id, :ministry_involvement)} = ? AND (#{_(:ministry_role_id, :ministry_involvement)} IN (?) OR admin = 1) AND #{_(:end_date, :ministry_involvement)} is null",
+             id, ::Ministry.first.root.staff_role_ids]).present?
+      end
+
       protected
       def update_stamp
         self.updated_at = Time.now
@@ -365,11 +371,6 @@ module Common
         people
       end
 
-      def is_staff_somewhere?
-        MinistryInvolvement.find(:first, :conditions =>
-           ["#{_(:person_id, :ministry_involvement)} = ? AND (#{_(:ministry_role_id, :ministry_involvement)} IN (?) OR admin = 1) AND #{_(:end_date, :ministry_involvement)} is null",
-             id, Ministry.first.root.staff_role_ids]).present?
-      end
     end
   end
 end
