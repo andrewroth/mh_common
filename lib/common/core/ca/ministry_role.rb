1f2603b65f0f8861add7a91e445a403d9ddba6a3
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-05-06 11:11:43 -0500

    Add generic ministry_roles_that_grant_access method

--- lib/common/core/ca/ministry_role.rb
+++ lib/common/core/ca/ministry_role.rb
@@ -9,8 +9,8 @@ module Common
 
       module MinistryRoleClassMethods
 
-        def ministry_roles_that_grant_stats_access
-          role_ids = ::MinistryRolePermission.all(:joins => :permission, :conditions => ["#{::Permission.table_name}.controller = ? AND #{::Permission.table_name}.action = ?", "stats", "index"]).collect{ |mrp| mrp.ministry_role_id }
+        def ministry_roles_that_grant_access(controller, action)
+          role_ids = ::MinistryRolePermission.all(:joins => :permission, :conditions => ["#{::Permission.table_name}.controller = ? AND #{::Permission.table_name}.action = ?", controller, action]).collect{ |mrp| mrp.ministry_role_id }
           ::MinistryRole.all(:conditions => ["id IN(?)", role_ids])
         end
 
