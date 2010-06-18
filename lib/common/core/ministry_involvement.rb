0ce6edf1cf0a1ee0bc49bd526bde1be70519e469
Andrew Roth <andrewroth@gmail.com>
2010-06-18 14:19:21 -0400

    move the new_*_history methods to CDM

--- lib/common/core/ministry_involvement.rb
+++ lib/common/core/ministry_involvement.rb
@@ -27,6 +27,9 @@ module Common
     
       def archived?() end_date.present? end
 
+      def new_staff_history
+        ::StaffInvolvementHistory.new :person_id => person_id, :end_date => Date.today, :ministry_role_id => ministry_role_id, :start_date => (last_history_update_date || start_date), :ministry_involvement_id => id, :ministry_id => ministry_id
+      end
     end
   end
 end
