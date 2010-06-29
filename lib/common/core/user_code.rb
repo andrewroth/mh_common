dda24fc91ce8ade156591485e4b7ba52cd71dc2b
Andrew Roth <andrewroth@gmail.com>
2010-06-29 16:05:17 -0400

    add user_code

---
+++ lib/common/core/user_code.rb
@@ -0,0 +1,27 @@
+module Common
+  module Core
+    module UserCode
+      def self.included(base)
+        base.class_eval do
+          belongs_to :user
+        end
+
+        base.extend UserCodeClassMethods
+      end
+
+      def callback_url(base_url, controller, action)
+        "#{base_url}/user_codes/#{code}/#{controller}/#{action}"
+      end
+
+      def pass_hash
+        Marshal.load(pass)
+      end
+
+      module UserCodeClassMethods
+        def new_code
+          MD5.hexdigest((object_id + Time.now.to_i).to_s)
+        end
+      end
+    end
+  end
+end
