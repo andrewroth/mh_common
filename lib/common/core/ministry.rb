1e7b6216b4245d59bd5d9f71169fa4567c272440
Andrew Roth <andrewroth@gmail.com>
2010-08-31 21:08:19 -0400

    use top-level ref ::

--- lib/common/core/ministry.rb
+++ lib/common/core/ministry.rb
@@ -311,13 +311,13 @@ module Common
 
       def descendants_staff_count_hash
         return @descendants_staff_count_hash if @descendants_staff_count_hash
-        ms = Ministry.find(:all, :select => "#{Ministry.__(:id)}, (COUNT(*)) as num_staff", :joins => "INNER JOIN #{MinistryInvolvement.table_name} ON #{MinistryInvolvement.__(:ministry_id)} = #{Ministry.__(:id)}", :conditions => self.descendants_condition + " AND ministry_role_id IN (#{self.staff_role_ids.join(',')})", :group => Ministry.__(:id))
+        ms = ::Ministry.find(:all, :select => "#{::Ministry.__(:id)}, (COUNT(*)) as num_staff", :joins => "INNER JOIN #{::MinistryInvolvement.table_name} ON #{::MinistryInvolvement.__(:ministry_id)} = #{::Ministry.__(:id)}", :conditions => self.descendants_condition + " AND ministry_role_id IN (#{self.staff_role_ids.join(',')})", :group => ::Ministry.__(:id))
         @descendants_staff_count_hash = Hash[ms.collect{ |m| [ m.id, m.num_staff ] }]
       end
 
       def descendants_groups_count_hash
         return @descendants_groups_count_hash if @descendants_groups_count_hash
-        ms = Ministry.find(:all, :select => "#{Ministry.__(:id)}, (COUNT(*)) as num_groups", :joins => "INNER JOIN #{Group.table_name} ON #{Group.__(:ministry_id)} = #{Ministry.__(:id)}", :conditions => self.descendants_condition, :group => Ministry.__(:id))
+        ms = ::Ministry.find(:all, :select => "#{::Ministry.__(:id)}, (COUNT(*)) as num_groups", :joins => "INNER JOIN #{::Group.table_name} ON #{::Group.__(:ministry_id)} = #{::Ministry.__(:id)}", :conditions => self.descendants_condition, :group => ::Ministry.__(:id))
         @descendants_groups_count_hash = Hash[ms.collect{ |m| [ m.id, m.num_groups ] }]
       end
 
