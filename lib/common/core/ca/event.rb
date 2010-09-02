eaad5bf8d4b284d34c9c0fa6b6430ae7833fd391
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-09-02 13:50:09 -0400

    add event.rb

---
+++ lib/common/core/ca/event.rb
@@ -0,0 +1,42 @@
+module Common
+  module Core
+    module Ca
+      module Event
+        def self.included(base)
+          base.class_eval do
+            def eventbrite_id() self.registrar_event_id end
+          end
+
+          def all_attendees_from_campus(campus)
+            attendees = []
+
+            EventBright.setup(EventBright::KEYS[:api], true)
+            @eventbrite_user = EventBright::User.new(EventBright::KEYS[:user])
+
+            eb_event = EventBright::Event.new(@eventbrite_user, {:id => self.eventbrite_id})
+
+            eb_event.attendees.each do |attendee|
+              if attendee.answers then
+                attendee.answers.each do |answer|
+                  answer = answer["answer"]
+                  if answer["question"] == "Your Campus"
+                    # answer_text should be in the format "campus.desc (campus.short_desc)"
+                    # if either campus.desc or campus.short_desc matches then true
+                    if answer["answer_text"].slice(answer["answer_text"].rindex("("), answer["answer_text"].rindex(")")) == "(#{campus.short_desc})" ||
+                        answer["answer_text"].slice(0, answer["answer_text"].rindex("(") - 1) == "#{campus.desc}"
+
+                        attendees << attendee
+                    end
+                  end
+                end
+              end
+            end
+
+            attendees
+          end
+        end
+
+      end
+    end
+  end
+end
