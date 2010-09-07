a8f59745b6ca58f5944ce92e3f300ec8f99a6b85
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-09-07 10:55:32 -0400

    take advantage of answer_to_question method

--- lib/common/core/ca/event.rb
+++ lib/common/core/ca/event.rb
@@ -16,16 +16,15 @@ module Common
 
             eb_event.attendees.each do |attendee|
               if attendee.answers then
-                attendee.answers.each do |answer|
-                  answer = answer["answer"]
-                  if answer["question"] == "Your Campus"
-                    # answer_text should be in the format "campus.desc (campus.short_desc)"
-                    # if either campus.desc or campus.short_desc matches then true
-                    if answer["answer_text"].slice(answer["answer_text"].rindex("("), answer["answer_text"].rindex(")")) == "(#{campus.short_desc})" ||
-                        answer["answer_text"].slice(0, answer["answer_text"].rindex("(") - 1) == "#{campus.desc}"
+                answer = attendee.answer_to_question(eventbrite[:campus_question])
 
-                        attendees << attendee
-                    end
+                if answer
+                  # answer_text should be in the format "campus.desc (campus.short_desc)"
+                  # if either campus.desc or campus.short_desc matches then true
+                  if answer.slice(answer.rindex("("), answer.rindex(")")) == "(#{campus.short_desc})" ||
+                     answer.slice(0, answer.rindex("(") - 1) == "#{campus.desc}"
+
+                      attendees << attendee
                   end
                 end
               end
