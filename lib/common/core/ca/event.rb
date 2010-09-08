47e49bb5d5093bf59c1d5ca9fd74794009f29536
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-09-08 13:37:54 -0400

    add eventbrite methods

--- lib/common/core/ca/event.rb
+++ lib/common/core/ca/event.rb
@@ -6,32 +6,36 @@ module Common
           base.class_eval do
             def eventbrite_id() self.registrar_event_id end
           end
+        end
 
-          def all_attendees_from_campus(campus)
-            attendees = []
 
-            @eventbrite_user = EventBright.setup_from_initializer()
+        def all_attendees_from_campus(campus)
+          attendees = []
 
-            eb_event = EventBright::Event.new(@eventbrite_user, {:id => self.eventbrite_id})
+          @eventbrite_user = EventBright.setup_from_initializer()
 
-            eb_event.attendees.each do |attendee|
-              if attendee.answers then
-                answer = attendee.answer_to_question(eventbrite[:campus_question])
+          eb_event = EventBright::Event.new(@eventbrite_user, {:id => self.eventbrite_id})
 
-                if answer
-                  # answer_text should be in the format "campus.desc (campus.short_desc)"
-                  # if either campus.desc or campus.short_desc matches then true
-                  if answer.slice(answer.rindex("("), answer.rindex(")")) == "(#{campus.short_desc})" ||
-                     answer.slice(0, answer.rindex("(") - 1) == "#{campus.desc}"
+          eb_event.attendees.each do |attendee|
+            if attendee.answers then
+              answer = attendee.answer_to_question(eventbrite[:campus_question])
 
-                      attendees << attendee
-                  end
+              if answer
+                # answer_text should be in the format "campus.desc (campus.short_desc)"
+                # if either campus.desc or campus.short_desc matches then true
+                if campus.matches_eventbrite_campus(answer)
+                  attendees << attendee
                 end
+#                  if answer.slice(answer.rindex("("), answer.rindex(")")) == "(#{campus.short_desc})" ||
+#                     answer.slice(0, answer.rindex("(") - 1) == "#{campus.desc}"
+#
+#                      attendees << attendee
+#                  end
               end
             end
-
-            attendees
           end
+
+          attendees
         end
 
       end
