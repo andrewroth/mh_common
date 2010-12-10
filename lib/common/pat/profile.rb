47c52116db310a7fbb66df3d8549b49d2db15c8e
Andrew Roth <andrewroth@gmail.com>
2010-12-10 15:54:06 -0500

    use user instead of viewer

--- lib/common/pat/profile.rb
+++ lib/common/pat/profile.rb
@@ -2,7 +2,7 @@ module Common::Pat::User
   def self.included(base)
     base.class_eval do
       belongs_to :project
-      belongs_to :viewer
+      belongs_to :user, :foreign_key => "viewer_id"
     end
   end
 end
