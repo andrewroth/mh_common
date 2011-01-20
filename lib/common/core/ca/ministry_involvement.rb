5ed49a95878a271690dc387c5af4abf2b79eaf3f
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-01-20 14:27:18 -0500

    update person's campus involvement if min involvement changed to alumni

--- lib/common/core/ca/ministry_involvement.rb
+++ lib/common/core/ca/ministry_involvement.rb
@@ -12,7 +12,12 @@ module Common
 
         def set_graduated_school_year_if_involved_alumni
           if self.ministry_role.name == "Alumni"
-            
+            # automatically set the person's campus involvements to be graduated
+            graduated = ::SchoolYear.first(:conditions => ["#{::SchoolYear._(:name)} = ?", "Graduated"])
+            self.person.campus_involvements.each do |ci|
+              ci.school_year = graduated
+              ci.save
+            end
           end
         end
 
