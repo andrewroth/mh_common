f63f203a75b04af343dfc418d6e17be738f1181c
Hobbe Smit <hobbe.smit@c4c.ca>
2011-01-28 12:24:28 -0500

    Added tests for mentorship on profile page

--- test/factories/profile_pictures.rb
+++ test/factories/profile_pictures.rb
@@ -40,3 +40,38 @@ Factory.define :profilepicture_4, :class => ProfilePicture do |p|
   p.filename 'picture_medium.jpg'
   p.thumbnail 'medium'
 end
+
+
+Factory.define :profilepicture_6, :class => ProfilePicture do |p|
+  p.id '6'
+  p.person_id '4001'
+  p.size '5'
+  p.height '3'
+  p.width '8'
+  p.content_type 'image/jpeg'
+  p.filename 'picture.jpg'
+end
+
+
+
+Factory.define :profilepicture_20, :class => ProfilePicture do |p|
+  p.id '20'
+  p.person_id '3000'
+  p.size '5'
+  p.height '3'
+  p.width '8'
+  p.content_type 'image/jpeg'
+  p.filename 'picture_mini.jpg'
+  p.thumbnail 'mini'
+end
+
+Factory.define :profilepicture_30, :class => ProfilePicture do |p|
+  p.id '30'
+  p.person_id '2000'
+  p.size '5'
+  p.height '3'
+  p.width '8'
+  p.content_type 'image/jpeg'
+  p.filename 'picture_medium.jpg'
+  p.thumbnail 'medium'
+end
