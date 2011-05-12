26e9e8b0f7d567e6053a710a07ff2be5a202eece
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-05-12 17:05:55 -0400

    abstract codes in user_code to login_codes

--- lib/common/core/user_code.rb
+++ lib/common/core/user_code.rb
@@ -4,22 +4,35 @@ module Common
       def self.included(base)
         base.class_eval do
           belongs_to :user
+          belongs_to :login_code
+          
+          after_initialize :init
+          after_create :init
         end
 
         base.extend UserCodeClassMethods
       end
 
       def callback_url(base_url, controller, action)
-        "#{base_url}/user_codes/#{code}/#{controller}/#{action}"
+        "#{base_url}/user_codes/#{self.login_code.code}/#{controller}/#{action}"
       end
 
       def pass_hash
         Marshal.load(pass)
       end
 
+      private
+      
+      def init
+        lc = ::LoginCode.new
+        lc.save!
+        self.login_code_id = lc.id
+        self.save!
+      end
+
       module UserCodeClassMethods
         def new_code
-          MD5.hexdigest((object_id + Time.now.to_i).to_s)
+          ::LoginCode.new_code
         end
       end
     end
