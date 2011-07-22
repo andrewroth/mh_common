71ed8ad529a0a031ce2945f5c13572e1f95a46a8
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-07-22 16:54:35 -0500

    correct typo

--- test/factories/event_attendees.rb
+++ test/factories/event_attendees.rb
@@ -2,7 +2,7 @@ Factory.define :event_1_attendee_1, :class => EventAttendee, :singleton => true 
   e.id '1'
   e.event_id '1'
   e.ticket_id '11'
-  e.ticket_update_at DateTime.new(2010, 5, 1, 9)
+  e.ticket_updated_at DateTime.new(2010, 5, 1, 9)
   e.email 'fred@uscm.org'
   e.first_name 'Fred'
   e.last_name 'Anderson'
@@ -18,7 +18,7 @@ Factory.define :event_1_attendee_2, :class => EventAttendee, :singleton => true 
   e.id '2'
   e.event_id '1'
   e.ticket_id '12'
-  e.ticket_update_at DateTime.new(2010, 4, 12, 10)
+  e.ticket_updated_at DateTime.new(2010, 4, 12, 10)
   e.email 'someone.not.in.db@3r98hjewfsdfk4wiuh.com'
   e.first_name 'Someone'
   e.last_name 'Somewhere'
