9bb25e0183b41a7b5e590e338aa16583d0081691
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-10-06 17:26:01 -0400

    add campus info to tests

--- test/factories/ca/campuses.rb
+++ test/factories/ca/campuses.rb
@@ -3,9 +3,12 @@ Factory.define :campus_1, :class => Campus, :singleton => true do |c|
   c.name 'University of California-Davis'
   c.short_desc 'UoCD'
   c.province_id '1'
+  c.campus_website 'www.ucdavis.edu'
   c.campus_facebookgroup ''
   c.campus_gcxnamespace ''
   c.region_id '5'
+  c.longitude '-121.75'
+  c.latitude '38.54'
 end
 
 Factory.define :campus_2, :class => Campus, :singleton => true do |c|
@@ -13,9 +16,12 @@ Factory.define :campus_2, :class => Campus, :singleton => true do |c|
   c.name 'Sacramento State'
   c.short_desc 'SS'
   c.province_id '1'
+  c.campus_website 'www.csus.edu'
   c.campus_facebookgroup ''
   c.campus_gcxnamespace ''
   c.region_id '5'
+  c.longitude '-121.424111'
+  c.latitude '38.560222'
 end
 
 Factory.define :campus_3, :class => Campus, :singleton => true do |c|
@@ -23,9 +29,12 @@ Factory.define :campus_3, :class => Campus, :singleton => true do |c|
   c.name 'Campus of Wyoming'
   c.short_desc 'CoW'
   c.province_id '2'
+  c.campus_website ''
   c.campus_facebookgroup ''
   c.campus_gcxnamespace ''
   c.region_id '6'
+  c.longitude '-105.581389'
+  c.latitude '41.313056'
 end
 
 Factory.define :campus_4, :class => Campus, :singleton => true do |c|
@@ -33,8 +42,11 @@ Factory.define :campus_4, :class => Campus, :singleton => true do |c|
   c.name 'National'
   c.short_desc 'Nat'
   c.province_id '0'
+  c.campus_website 'powertochange.com'
   c.campus_facebookgroup ''
   c.campus_gcxnamespace ''
   c.region_id '4'
+  c.longitude '-122.65758'
+  c.latitude '49.1041'
 end
 
